88e9b14838d7152ad74694067ff450b8da224397
PGK79 <pgk_nev@mail.ru>
2023-01-25 12:16:13 +0500

    Добавлен конструктор

--- src/main/java/ru/netology/authorizationservice/services/AuthorizationService.java
+++ src/main/java/ru/netology/authorizationservice/services/AuthorizationService.java
@@ -12,6 +12,10 @@ import java.util.List;
 public class AuthorizationService {
     UserRepository userRepository;
 
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public List<Authorities> getAuthorities(String user, String password) {
         if (isEmpty(user) || isEmpty(password)) {
             throw new InvalidCredentials("User name or password is empty");
@@ -21,6 +25,7 @@ public class AuthorizationService {
             throw new UnauthorizedUser("Unknown user " + user);
         }
         return userAuthorities;
+
     }
 
     private boolean isEmpty(String str) {
