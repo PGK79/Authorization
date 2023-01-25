6486c15c3bfd9829d857713cd987416781726266
PGK79 <pgk_nev@mail.ru>
2023-01-25 12:15:58 +0500

    Добавлена логика поиска разрешений по имени и логину

--- src/main/java/ru/netology/authorizationservice/repositories/UserRepository.java
+++ src/main/java/ru/netology/authorizationservice/repositories/UserRepository.java
@@ -3,11 +3,23 @@ package ru.netology.authorizationservice.repositories;
 import org.springframework.stereotype.Repository;
 import ru.netology.authorizationservice.enums.Authorities;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class UserRepository {
+    Map<String, List<Authorities>> permissionStore = Map.of("Ivan123", List.of(Authorities.READ,
+                    Authorities.WRITE), "Olga456", List.of(Authorities.READ, Authorities.DELETE),
+            "Petya789", List.of(Authorities.READ, Authorities.WRITE, Authorities.DELETE));
+
     public List<Authorities> getUserAuthorities(String user, String password) {
-        return ;
+        String keyForAdmission = user + password;
+        if (permissionStore.containsKey(keyForAdmission)) {
+            List<Authorities> list = permissionStore.get(keyForAdmission);
+            return list;
+        } else {
+            return new ArrayList<>();
+        }
     }
 }
